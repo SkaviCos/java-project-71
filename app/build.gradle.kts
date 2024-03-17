8865650fdcf54bb1cb27e2f9372954626ca75f0c
skavicos <kskavinsky@skolopendra.com>
2024-03-17 21:47:57 +0300

    add make

---
+++ app/build.gradle.kts
@@ -0,0 +1,31 @@
+plugins {
+    id("java")
+    application
+    checkstyle
+    java
+}
+
+application {
+    mainClass = "hexlet.code.App"
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+}
+
+tasks.getByName("run", JavaExec::class) {
+    standardInput = System.`in`
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
