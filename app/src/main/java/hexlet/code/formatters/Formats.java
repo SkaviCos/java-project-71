5d473ab4bc129d3b593ee0537da02aacbe1561ea
skavicos <kskavinsky@skolopendra.com>
2024-04-14 15:51:44 +0300

    fix tests after review

---
+++ app/src/main/java/hexlet/code/formatters/Formats.java
@@ -0,0 +1,19 @@
+package hexlet.code.formatters;
+
+public enum Formats {
+
+
+    JSON("json"),
+    PLAIN("plain"),
+    STYLISH("stylish");
+    private final String value;
+
+    Formats(String value) {
+        this.value = value;
+    }
+
+    public String getValue() {
+        return value;
+    }
+}
+
--- app/src/test/java/code/DifferTest.java
+++ app/src/test/java/code/DifferTest.java
@@ -7,56 +7,64 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+import static hexlet.code.formatters.Formats.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
-    private final String path1 = "src/test/resources/step81.json";
-    private final String path2 = "src/test/resources/step82.json";
-    private final String path3 = "src/test/resources/step81.yml";
-    private final String path4 = "src/test/resources/step82.yml";
+    private static final String path1 = "src/test/resources/dataJsonFirst.json";
+    private static final String path2 = "src/test/resources/dataJsonSecond.json";
+    private static final String path3 = "src/test/resources/dataYamlFirst.yml";
+    private static final String path4 = "src/test/resources/dataYamlSecond.yml";
 
-    private final Path pathStylish =
-            Paths.get("src/test/resources/expected/testStylish").toAbsolutePath().normalize();
-    private final Path pathPlain =
-            Paths.get("src/test/resources/expected/testPlain").toAbsolutePath().normalize();
-    private final Path pathJson =
-            Paths.get("src/test/resources/expected/testJson").toAbsolutePath().normalize();
+    private static final String pathStylish = "src/test/resources/expected/testStylish";
+    private static final String pathJson = "src/test/resources/expected/testJson";
+    private static final String pathPlain = "src/test/resources/expected/testPlain";
 
+    private static Path normalize(String path) {
+        return Paths.get(path).toAbsolutePath().normalize();
+    }
+
+    //TODO сделать параметеризороваанные тесты
+    @Test
+    public void testDefaultFormat() throws Exception {
+        String expected = Files.readString(normalize(pathStylish));
+        assertEquals(expected, Differ.generate(path1, path2));
+    }
     @Test
     public void testJsonStyleStylish() throws Exception {
-        String expected = Files.readString(pathStylish);
-        assertEquals(expected, Differ.generate(path1, path2, "stylish"));
+        String expected = Files.readString(normalize(pathStylish));
+        assertEquals(expected, Differ.generate(path1, path2, STYLISH.getValue()));
     }
 
     @Test
     public void testYamlStyleStylish() throws Exception {
-        String expected = Files.readString(pathStylish);
-        assertEquals(expected, Differ.generate(path3, path4, "stylish"));
+        String expected = Files.readString(normalize(pathStylish));
+        assertEquals(expected, Differ.generate(path3, path4, STYLISH.getValue()));
     }
 
     @Test
     public void testJsonStylePlain() throws Exception {
-        String expected = Files.readString(pathPlain);
-        assertEquals(expected, Differ.generate(path1, path2, "plain"));
+        String expected = Files.readString(normalize(pathPlain));
+        assertEquals(expected, Differ.generate(path1, path2, PLAIN.getValue()));
     }
 
     @Test
     public void testYamlStylePlain() throws Exception {
-        String expected = Files.readString(pathPlain);
-        assertEquals(expected, Differ.generate(path3, path4, "plain"));
+        String expected = Files.readString(normalize(pathPlain));
+        assertEquals(expected, Differ.generate(path3, path4, PLAIN.getValue()));
     }
 
     @Test
     public void testJsonStyleJson() throws Exception {
-        String expected = Files.readString(pathJson);
-        assertEquals(expected, Differ.generate(path1, path2, "json"));
+        String expected = Files.readString(normalize(pathJson));
+        assertEquals(expected, Differ.generate(path1, path2, JSON.getValue()));
     }
 
     @Test
     public void testYamlStyleJson() throws Exception {
-        String expected = Files.readString(pathJson);
-        assertEquals(expected, Differ.generate(path3, path4, "json"));
+        String expected = Files.readString(normalize(pathJson));
+        assertEquals(expected, Differ.generate(path3, path4, JSON.getValue()));
     }
 
 
