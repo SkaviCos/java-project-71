68b96d8299a4542fa1e160ffeaf48b92d11f5ed3
skavicos <kskavinsky@skolopendra.com>
2024-04-14 22:51:48 +0300

    add parameterized test

--- app/src/test/java/code/DifferTest.java
+++ app/src/test/java/code/DifferTest.java
@@ -2,10 +2,14 @@ package code;
 
 import hexlet.code.Differ;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.stream.Stream;
 
 import static hexlet.code.formatters.Formats.JSON;
 import static hexlet.code.formatters.Formats.PLAIN;
@@ -14,10 +18,10 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class DifferTest {
 
-    private static final String PATH_1 = "src/test/resources/dataJsonFirst.json";
-    private static final String PATH_2 = "src/test/resources/dataJsonSecond.json";
-    private static final String PATH_3 = "src/test/resources/dataYamlFirst.yml";
-    private static final String PATH_4 = "src/test/resources/dataYamlSecond.yml";
+    private static final String JSON_FIRST = "src/test/resources/dataJsonFirst.json";
+    private static final String JSON_SECOND = "src/test/resources/dataJsonSecond.json";
+    private static final String YAML_FIRST = "src/test/resources/dataYamlFirst.yml";
+    private static final String YAML_SECOND = "src/test/resources/dataYamlSecond.yml";
 
     private static final String PATH_STYLISH = "src/test/resources/expected/testStylish";
     private static final String PATH_JSON = "src/test/resources/expected/testJson";
@@ -27,48 +31,27 @@ public class DifferTest {
         return Paths.get(path).toAbsolutePath().normalize();
     }
 
-    //TODO сделать параметеризороваанные тесты
+    private static Stream<Arguments> testStyle() {
+        return Stream.of(
+                Arguments.of(JSON_FIRST, JSON_SECOND, STYLISH.getValue(), PATH_STYLISH),
+                Arguments.of(YAML_FIRST, YAML_SECOND, STYLISH.getValue(), PATH_STYLISH),
+                Arguments.of(JSON_FIRST, JSON_SECOND, PLAIN.getValue(), PATH_PLAIN),
+                Arguments.of(YAML_FIRST, YAML_SECOND, PLAIN.getValue(), PATH_PLAIN),
+                Arguments.of(JSON_FIRST, JSON_SECOND, JSON.getValue(), PATH_JSON),
+                Arguments.of(YAML_FIRST, YAML_SECOND, JSON.getValue(), PATH_JSON)
+        );
+    }
+
     @Test
     public void testDefaultFormat() throws Exception {
         String expected = Files.readString(normalize(PATH_STYLISH));
-        assertEquals(expected, Differ.generate(PATH_1, PATH_2));
+        assertEquals(expected, Differ.generate(JSON_FIRST, JSON_SECOND));
     }
 
-    @Test
-    public void testJsonStyleStylish() throws Exception {
-        String expected = Files.readString(normalize(PATH_STYLISH));
-        assertEquals(expected, Differ.generate(PATH_1, PATH_2, STYLISH.getValue()));
+    @ParameterizedTest(name = "{index} Style {2} test with files {0} and {1}")
+    @MethodSource("testStyle")
+    public void styleTest(String path1, String path2, String format, String expectedStyle) throws Exception {
+        String expected = Files.readString(normalize(expectedStyle));
+        assertEquals(expected, Differ.generate(path1, path2, format));
     }
-
-    @Test
-    public void testYamlStyleStylish() throws Exception {
-        String expected = Files.readString(normalize(PATH_STYLISH));
-        assertEquals(expected, Differ.generate(PATH_3, PATH_4, STYLISH.getValue()));
-    }
-
-    @Test
-    public void testJsonStylePlain() throws Exception {
-        String expected = Files.readString(normalize(PATH_PLAIN));
-        assertEquals(expected, Differ.generate(PATH_1, PATH_2, PLAIN.getValue()));
-    }
-
-    @Test
-    public void testYamlStylePlain() throws Exception {
-        String expected = Files.readString(normalize(PATH_PLAIN));
-        assertEquals(expected, Differ.generate(PATH_3, PATH_4, PLAIN.getValue()));
-    }
-
-    @Test
-    public void testJsonStyleJson() throws Exception {
-        String expected = Files.readString(normalize(PATH_JSON));
-        assertEquals(expected, Differ.generate(PATH_1, PATH_2, JSON.getValue()));
-    }
-
-    @Test
-    public void testYamlStyleJson() throws Exception {
-        String expected = Files.readString(normalize(PATH_JSON));
-        assertEquals(expected, Differ.generate(PATH_3, PATH_4, JSON.getValue()));
-    }
-
-
 }
